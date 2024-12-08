ee7e4b11f29c426b9bb7870971160933e28d9246
vik-j <114836259+vik-j@users.noreply.github.com>
2024-12-08 14:46:41 -0800

    Update TwoWheelLocalizer.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/localizers/TwoWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/localizers/TwoWheelLocalizer.java
@@ -81,8 +81,8 @@ public class TwoWheelLocalizer extends Localizer { // todo: make two wheel odo w
      */
     public TwoWheelLocalizer(HardwareMap map, Pose setStartPose) {
         // TODO: replace these with your encoder positions
-        forwardEncoderPose = new Pose(-18.5/25.4 - 0.1, 164.4/25.4, 0);
-        strafeEncoderPose = new Pose(-107.9/25.4+0.25, -1.1/25.4-0.23, Math.toRadians(90));
+        forwardEncoderPose = new Pose(3.625, -1.25, 0);
+        strafeEncoderPose = new Pose(2.5, 1.5, Math.toRadians(90));
 
         hardwareMap = map;
 
