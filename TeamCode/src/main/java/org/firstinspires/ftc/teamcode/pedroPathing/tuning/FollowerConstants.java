e1b1f36beebe5400d86c9450ec77ed76ff6d4444
vik-j <114836259+vik-j@users.noreply.github.com>
2024-12-08 15:32:58 -0800

    motors dirs and mass

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/tuning/FollowerConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/tuning/FollowerConstants.java
@@ -83,7 +83,7 @@ public class FollowerConstants {
 
 
     // Mass of robot in kilograms
-    public static double mass = 10.65942;
+    public static double mass = 4.85;
 
     // Centripetal force to power scaling
     public static double centripetalScaling = 0.0005;
